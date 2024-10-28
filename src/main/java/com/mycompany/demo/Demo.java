d32e8ffc1383a02568107594e2644903b08a2f13
nhadinh00 <NguyenCatNhaDinhh@gmail.com>
2024-10-28 21:27:38 +0700

    Update Demo.java

--- src/main/java/com/mycompany/demo/Demo.java
+++ src/main/java/com/mycompany/demo/Demo.java
@@ -11,6 +11,6 @@ package com.mycompany.demo;
 public class Demo {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello cc");
     }
 }
